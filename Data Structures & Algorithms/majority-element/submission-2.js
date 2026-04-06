bb98d8f8987a75ebb6cc383c0da643b5a763b783
Jeeva Kalaiselvam <77735712+jeevakalaiselvam@users.noreply.github.com>
2026-04-06 07:49:43 +0530

    Add: majority-element - submission-2

---
+++ Data Structures & Algorithms/majority-element/submission-2.js
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    majorityElement(nums) {
+        let candidate = 0 ;
+        let count = 0;
+        for(let num of nums){
+            if(count == 0) candidate = num;
+            if(candidate == num) count++;
+            else count--;
+        }
+        return candidate;
+    }
+}
