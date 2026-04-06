5c95fc593eff9c0be3b80e95aad939977a619a1e
Jeeva Kalaiselvam <77735712+jeevakalaiselvam@users.noreply.github.com>
2026-04-06 21:18:51 +0530

    Add: top-k-elements-in-list - submission-1

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.js
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums, k) {
+        let freqMap = new Map();
+
+        for(let num of nums){
+            freqMap.set(num,(freqMap.get(num) || 0) + 1)
+        }
+
+        let buckets = Array.from({length: nums.length + 1}, () => []);
+
+        for(let [num,freq] of freqMap){
+            buckets[freq].push(Number(num))
+        }
+
+        let results = [];
+
+        for(let i = buckets.length - 1; i >= 0 && results.length < k; i--){
+            for(let num of buckets[i]){
+                results.push(num);
+                if(results.length === k) break;
+            }
+        }
+
+        return results;
+
+    }
+}
