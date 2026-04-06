bb71e42f248593756692ed96ff310b1285b52e6c
Jeeva Kalaiselvam <77735712+jeevakalaiselvam@users.noreply.github.com>
2026-04-06 19:49:03 +0530

    Add: sort-an-array - submission-6

---
+++ Data Structures & Algorithms/sort-an-array/submission-6.js
@@ -0,0 +1,44 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+
+    swap(nums,i,j){
+        let tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+
+    partition(nums,low,high){
+        let pivot = nums[low]
+        let i = low - 1;
+        let j = high + 1;
+
+        while(true){
+            do{
+                i++;
+            }while(nums[i] < pivot)
+            do{
+                j--;
+            }while(nums[j] > pivot)
+            if(i >= j){
+                return j;
+            }
+            this.swap(nums,i,j)
+        }
+    }
+
+    quicksort(nums,low,high){
+        if(low < high){
+            let j = this.partition(nums,low,high);
+            this.quicksort(nums,low,j);
+            this.quicksort(nums,j + 1, high);
+        }
+    }
+
+    sortArray(nums) {
+        this.quicksort(nums,0,nums.length - 1)
+        return nums;
+    }
+}
