d718d236c2af4fe76a62004fd2e2f99d3964c18c
Jeeva Kalaiselvam <77735712+jeevakalaiselvam@users.noreply.github.com>
2026-04-06 20:50:54 +0530

    Add: sort-colors - submission-1

---
+++ Data Structures & Algorithms/sort-colors/submission-1.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {void} Do not return anything, modify nums in-place instead.
+     */
+    sortColors(nums) {
+        let low = 0;
+        let mid = 0;
+        let high = nums.length - 1;
+        while(mid <= high){
+            if(nums[mid] === 0){
+                this.swap(nums,low,mid);
+                low++;
+                mid++
+            }else if(nums[mid] === 1){
+                mid++;
+            }else{
+                this.swap(nums,mid,high);
+                high--
+            }
+        }
+        return nums;
+    }
+
+    swap(nums,i,j){
+        let tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+}
