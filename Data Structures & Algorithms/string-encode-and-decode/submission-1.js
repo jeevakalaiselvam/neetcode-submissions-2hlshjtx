dc54cdb245eda89031e473c3c240ec8be625bfb0
Jeeva Kalaiselvam <77735712+jeevakalaiselvam@users.noreply.github.com>
2026-04-10 10:20:19 +0530

    Add: string-encode-and-decode - submission-1

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-1.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    encode(strs) {
+        return strs.map(str => `${str.length}#${str}`).join("")
+    }
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    decode(str) {
+        let results = [];
+        let i = 0;
+        while(i < str.length){
+            let j = i;
+            while(str[j] !== "#") j++;
+            let length = parseInt(str.slice(i,j));
+            results.push(str.slice(j + 1, j + 1 + length));
+            i = j + 1 + length;
+        }
+        return results;
+    }
+}
